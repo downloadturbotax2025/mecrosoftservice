3d7b511553d0c6abaa76a2fb814ef201e979cf79
downloadturbotax2025 <jjiku867@gmail.com>
2025-07-12 12:06:01 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'microsoft365 com setup'
 
 
 # The full version, including alpha/beta/rc tags
